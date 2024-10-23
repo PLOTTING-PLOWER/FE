bba57186bf9e8052e8adb6b462077ef24aecea15
Amepistheo <kellylhji@gmail.com>
2024-10-23 22:57:56 +0900

    :recycle: refactor: Participant를 data class로 변경

--- app/src/main/java/com/example/plotting_fe/plogging/dto/Participant.kt
+++ app/src/main/java/com/example/plotting_fe/plogging/dto/Participant.kt
@@ -1,19 +1,6 @@
-package com.example.plotting_fe.plogging.dto;
+package com.example.plotting_fe.plogging.dto
 
-public class Participant {
-    private String name;
-    private String details;
-
-    public Participant(String name, String details) {
-        this.name = name;
-        this.details = details;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-}
+data class Participant (
+    val name: String,
+    val details: String
+)
