6dab04a61c5bbeea4beab0bdea9e7c6f4ddd6dea
standyoonee <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-06 23:18:11 +0900

    💄style: 마이페이지 화면

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +44,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Material Design 라이브러리 (BottomNavigationView 사용을 위해 필요)
+    implementation("com.google.android.material:material:1.9.0")
+
+    // Navigation Component 라이브러리
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
+
+    // appcompat lib
+    implementation("androidx.appcompat:appcompat:1.3.0")
 }
