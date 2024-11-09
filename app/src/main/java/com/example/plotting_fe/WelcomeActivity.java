6384380d122695056b6a846f42ed7140ffb03146
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-10 02:37:00 +0900

    🚑 rename: 스플래시2 -> welcome 이름 변경

--- app/src/main/java/com/example/plotting_fe/WelcomeActivity.java
+++ app/src/main/java/com/example/plotting_fe/WelcomeActivity.java
@@ -5,18 +5,20 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.plotting_fe.utils.Utils;
 
-public class Splash2Activity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash2);
+        setContentView(R.layout.activity_welcome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Splash2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
