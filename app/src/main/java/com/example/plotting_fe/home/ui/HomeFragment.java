b9675c49253766fbe1985d1ed967b6fab03f7dd9
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-11 22:04:37 +0900

    💄 style: 랭킹 화면

--- app/src/main/java/com/example/plotting_fe/home/ui/HomeFragment.java
+++ app/src/main/java/com/example/plotting_fe/home/ui/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.plotting_fe;
+package com.example.plotting_fe.home.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.plotting_fe.R;
 
 /**
  * A simple {@link Fragment} subclass.
