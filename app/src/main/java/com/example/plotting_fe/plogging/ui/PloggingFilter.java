a32dd2a30a15b8cf3484f9ffe04ad76872f48be2
syjdjr <syjdjr@naver.com>
2024-11-17 01:27:28 +0900

    feat: 플로깅 모임 만들기 뷰1 뒤로 가기 버튼 제외하고 구현

---
+++ app/src/main/java/com/example/plotting_fe/plogging/ui/PloggingFilter.java
@@ -0,0 +1,18 @@
+package com.example.plotting_fe.plogging.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.plotting_fe.R;
+
+public class PloggingFilter extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_activity_filter);
+    }
+
+}
