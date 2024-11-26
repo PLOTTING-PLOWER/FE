5f4d71d70336ed9c077b642dd55f8430bb29ea1b
Amepistheo <kellylhji@gmail.com>
2024-11-26 23:45:48 +0900

    ♻️ refactor: 네비바 밑에 공백 없앰

--- app/src/main/java/com/example/plotting_fe/MainActivity.kt
+++ app/src/main/java/com/example/plotting_fe/MainActivity.kt
@@ -3,19 +3,24 @@ package com.example.plotting_fe
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.plotting_fe.databinding.AActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: AActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.a_activity_main)
+        _binding = AActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         // 시스템 바 인셋 처리
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
