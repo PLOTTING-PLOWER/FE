71ed067282ae8da55b6ad25c758cb4b12f0d8f3d
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-11-29 22:18:26 +0900

    🐛 fix: 머지하면서 사라진 코드 다시 추가

---
+++ app/src/main/java/com/example/plotting_fe/mypage/ui/StarViewPagerAdapter.java
@@ -0,0 +1,27 @@
+
+package com.example.plotting_fe.mypage.ui;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class StarViewPagerAdapter extends FragmentStateAdapter {
+    public StarViewPagerAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 0) {
+            return new StarPeopleFragment(); // 첫 번째 탭에 사람 목록 프래그먼트
+        } else {
+            return new StarPloggingFragment(); // 두 번째 탭에 플로깅 목록 프래그먼트
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2; // 탭의 개수
+    }
+}
